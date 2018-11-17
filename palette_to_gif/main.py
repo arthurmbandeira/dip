05508af628f26c73181ab6f7f34d38a93c00233d
Arthur <arthurmbandeira@gmail.com>
2018-11-17 01:51:30 -0200

    Write in a pythonic way.

--- palette_to_gif/main.py
+++ palette_to_gif/main.py
@@ -24,22 +24,22 @@ def generate_palette(h1, h2, num_frames):
 
     colors = np.linspace(h1, h2, num_frames)
 
-    sa = np.uint8(np.linspace(255, 0, 128))
     va = np.uint8(np.linspace(0, 255, 128))
+    sa = np.uint8(np.linspace(255, 0, 128))
     pad = np.uint8(np.linspace(255, 255, 128))
 
     lh = map(lambda x : np.uint8(np.full((256, 256), x)), colors)
 
-    lsa = np.uint8(np.tile(sa.reshape(128, 1), 256))
     lva = np.uint8(np.tile(va.reshape(128, 1), 256))
+    lsa = np.uint8(np.tile(sa.reshape(128, 1), 256))
     lpad = np.uint8(np.tile(pad.reshape(128, 1), 256))
 
     lv = np.concatenate((lva, lpad), axis=0)
     ls = np.concatenate((lpad, lsa), axis=0)
 
-    list_hsv = map(lambda x : np.dstack((np.dstack((x, ls)), lv)), lh)
+    map_hsv = map(lambda x : np.dstack((np.dstack((x, ls)), lv)), lh)
 
-    return np.array(list(list_hsv))
+    return np.array(list(map_hsv))
 
 def show_palette(palette):
     list(map(lambda x : im.fromarray(x, 'HSV').convert('RGB').show(), palette))
@@ -51,11 +51,5 @@ if __name__ == "__main__":
     palette = generate_palette(0, 300, len(gif_arr))
     # show_palette(palette[0:1])
 
-    run_arr = list(map(lambda x : np.array(im.fromarray(x).convert('RGB')), gif_arr))
-    out_arr = []
-
-    for x in range(len(gif_arr)):
-        run_arr[x] = palette[x][:, 1, :][gif_arr[x]]
-        out_arr.append(im.fromarray(run_arr[x], 'HSV').convert('RGB'))
-
-    out_arr[0].save('out_blow.gif', format='GIF', save_all=True, append_images=out_arr[1:], duration=100, loop=0)
+    out_arr = [im.fromarray(palette[x][:, 1, :][gif_arr[x]], 'HSV').convert('RGB') for x in range(len(gif_arr))]
+    out_arr[0].save('out_magic.gif', format='GIF', save_all=True, append_images=out_arr[1:], duration=100, loop=0)
